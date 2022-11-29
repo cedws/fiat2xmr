a3f9d139fa6742539750a3984ef51be6e79719ac
Connor Edwards <38229097+cedws@users.noreply.github.com>
2022-11-29 20:09:54 +0000

    Add README

---
+++ README.md
@@ -0,0 +1,4 @@
+# fiat2xmr
+Due to some fucking UK regulations it is not possible to buy one of the only private cryptocurrencies, Monero, on exchanges directly. It is however possible to purchase other currencies and convert them to Monero using services such as [SideShift](https://sideshift.ai). This tool aims to automate the process making it a single step to turn fiat in your Coinbase account into Monero deposited into your wallet. It should also highlight how utterly fucking stupid UK regulations can be.
+
+Work in progress.
--- cmd/root.go
+++ cmd/root.go
@@ -2,10 +2,8 @@ package cmd
 
 import (
 	"fmt"
-	"log"
 	"os"
 
-	"github.com/cedws/fiat2xmr/kraken"
 	"github.com/spf13/cobra"
 )
 
@@ -20,25 +18,14 @@ var (
 var rootCmd = &cobra.Command{
 	Use: "fiat2xmr",
 	Run: func(cmd *cobra.Command, args []string) {
-		client, _ := kraken.NewClient(apiKey, privateKey)
-		resp, err := client.AddOrder(kraken.AddOrderRequest{
-			Pair:      pair,
-			Type:      "buy",
-			OrderType: "market",
-			Volume:    fmt.Sprintf("%f", volumeBase),
-			Oflags:    []string{"fciq"},
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("%+v", resp)
+
 	},
 }
 
 func init() {
 	rootCmd.Flags().StringVarP(&apiKey, "api-key", "a", "", "kraken api key")
 	rootCmd.Flags().StringVarP(&privateKey, "private-key", "p", "", "kraken private key")
-	rootCmd.Flags().StringVar(&pair, "pair", "LTCGBP", "currency pair to use for conversion")
+	rootCmd.Flags().StringVar(&pair, "pair", "LTCXMR", "currency pair to use for conversion")
 	rootCmd.Flags().Float64VarP(&volumeBase, "volume-base", "b", 0, "volume to buy using base currency")
 	rootCmd.Flags().Float64VarP(&volumeQuote, "volume-quote", "q", 0, "volume to buy using quote currency")
 
