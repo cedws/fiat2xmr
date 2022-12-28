1f9be2a5fe07c4e3ddc8a31697528c79fb465739
Connor Edwards <38229097+cedws@users.noreply.github.com>
2022-12-28 21:52:02 +0000

    Add omitempty to request structs

--- coinbase/request.go
+++ coinbase/request.go
@@ -1,25 +1,25 @@
 package coinbase
 
 type TxRequest struct {
-	Type                        string `json:"type"`
-	To                          string `json:"to"`
-	Amount                      string `json:"amount"`
-	Currency                    string `json:"currency"`
-	SkipNotifications           bool   `json:"skip_notifications"`
-	Fee                         string `json:"fee"`
-	Nonce                       string `json:"nonce"`
-	ToFinancialInstitution      bool   `json:"to_financial_institution"`
-	FinancialInstitutionWebsite string `json:"financial_institution_website"`
+	Type                        string `json:"type,omitempty"`
+	To                          string `json:"to,omitempty"`
+	Amount                      string `json:"amount,omitempty"`
+	Currency                    string `json:"currency,omitempty"`
+	SkipNotifications           bool   `json:"skip_notifications,omitempty"`
+	Fee                         string `json:"fee,omitempty"`
+	Nonce                       string `json:"nonce,omitempty"`
+	ToFinancialInstitution      bool   `json:"to_financial_institution,omitempty"`
+	FinancialInstitutionWebsite string `json:"financial_institution_website,omitempty"`
 }
 
 type AdvancedCreateOrderRequest struct {
-	ClientOrderID      string `json:"client_order_id"`
-	ProductID          string `json:"product_id"`
-	Side               string `json:"side"`
+	ClientOrderID      string `json:"client_order_id,omitempty"`
+	ProductID          string `json:"product_id,omitempty"`
+	Side               string `json:"side,omitempty"`
 	OrderConfiguration struct {
 		MarketMarketIOC struct {
-			QuoteSize string `json:"quote_size"`
-			BaseSize  string `json:"base_size"`
-		} `json:"market_market_ioc"`
-	} `json:"order_configuration"`
+			QuoteSize string `json:"quote_size,omitempty"`
+			BaseSize  string `json:"base_size,omitempty"`
+		} `json:"market_market_ioc,omitempty"`
+	} `json:"order_configuration,omitempty"`
 }
